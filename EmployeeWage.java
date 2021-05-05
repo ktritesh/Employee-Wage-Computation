355501421370d7a3c42965d09cff1d4877f1cb51
ktritsh7 <ktriesh7@gmail.com>
2021-05-05 15:45:43 +0530

    UC1 Employee is Present or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,8 +3,14 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-
+		//Constants
+		int IS_PRESENT = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
